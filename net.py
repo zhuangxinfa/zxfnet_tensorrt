be8a97c269b8aa5f7c544332ffc9c2399e31da6e
zhuangxinfa <zhuangxinfa@126.com>
2020-05-11 18:12:38 +0800

    reconstruct the tensorflow graph

--- net.py
+++ net.py
@@ -9,14 +9,18 @@ import tensorflow as tf
 from tensorflow.python.framework import graph_util
 import numpy as np
 
-
+# (x+2)*3+4
 x = tf.placeholder(tf.float32,shape = [10,10,1],name = "x_placeholder")
 two = tf.constant(2.0,dtype = tf.float32,shape=[10,10,1],name = "two_op")
+three = tf.constant(3.0,dtype = tf.float32,shape=[1,1,1],name = "two_op")
+four = tf.constant(4.0,dtype = tf.float32,shape=[10,10,1],name = "two_op")
 y=tf.add(x,two,name = "add_op")
-sum = tf.mat5
+dot = tf.add(y,three,name = "dot_op")
+sum_fin = tf.add(dot,four,name="sum_fin_op") 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-print(sess.run(y,feed_dict={x:np.ones(shape=[10,10,1], dtype = float)}))
+
+print(sess.run(sum_fin,feed_dict={x:np.ones(shape=[10,10,1], dtype = float)}))
 # print(sess.run(two))
 graph_def = tf.get_default_graph().as_graph_def()
 
@@ -27,7 +31,7 @@ graph_def = tf.get_default_graph().as_graph_def()
 # 是要保存的计算图中的计算节点的名称，通常为字符串列表的形式，variable_names_whitelist
 # 是要转换为常量的变量名称集合(默认情况下，所有变量都将被转换)，
 # variable_names_blacklist是要省略转换为常量的变量名的集合。
-output_graph_def = graph_util.convert_variables_to_constants(sess, graph_def,output_node_names=["add_op"])
+output_graph_def = graph_util.convert_variables_to_constants(sess, graph_def,output_node_names=["sum_fin_op"])
 with tf.gfile.GFile("./test2x.pb", "wb") as f:
             # SerializeToString()函数用于将获取到的数据取出存到一个string对象中，
             # 然后再以二进制流的方式将其写入到磁盘文件中
